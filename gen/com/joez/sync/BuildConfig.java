c250e10effb1515f0392f054aeaa40718add7326
JoeZ <joez@xogrp.com>
2014-11-18 15:00:13 +0800

    ignore bin

---
+++ gen/com/joez/sync/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.joez.sync;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/com/joez/sync/MainActivity.java
+++ src/com/joez/sync/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		System.out.println("");
 		setContentView(R.layout.activity_main);
 	}
 	
